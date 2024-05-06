34e6f697447d4c8cf239d9a83476ae94bbb86e94
aryaan02 <aryaan.khan@yale.edu>
2024-05-06 15:03:16 -0400

    Create model module

---
+++ mudra_model.py
@@ -0,0 +1,28 @@
+import torch.nn as nn
+
+
+class CNN(nn.Module):
+    """
+    A simple CNN model for classifying the mudras.
+
+    Args:
+        num_classes (int): The number of classes in the dataset (default: 51)
+    """
+    def __init__(self, num_classes=51):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(64 * 28 * 28, 512)
+        self.fc2 = nn.Linear(512, num_classes)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.pool(self.relu(self.conv1(x)))
+        x = self.pool(self.relu(self.conv2(x)))
+        x = self.pool(self.relu(self.conv3(x)))
+        x = x.view(-1, 64 * 28 * 28)
+        x = self.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
